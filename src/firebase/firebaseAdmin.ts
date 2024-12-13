3762a7f2051848a8bf1e84694929b24684f8e012
kimchoker <kimchoker@naver.com>
2024-12-14 05:50:15 +0900

    시발 좀 돼라

--- src/firebase/firebaseAdmin.ts
+++ src/firebase/firebaseAdmin.ts
@@ -4,29 +4,16 @@ let adminDb: admin.database.Database | null = null;
 
 if (!admin.apps.length) {
   const serviceAccount: ServiceAccount = {
-    projectId: process.env.FIREBASE_ADMIN_PROJECT_ID!,
-    privateKey: (process.env.FIREBASE_ADMIN_PRIVATE_KEY! as string).replace(/\\n/g, "\n"),
-    clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL!,
+    projectId: process.env.PROJECT_ID!,
+    privateKey: (process.env.PRIVATE_KEY! as string).replace(/\\n/g, "\n"),
+    clientEmail: process.env.CLIENT_EMAIL!,
   };
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!,
+    databaseURL: process.env.DATABASE_URL!,
   });
-  console.log("Firebase Admin Apps:", admin.apps.length);
-  if (!process.env.FIREBASE_ADMIN_PROJECT_ID) {
-    console.error("FIREBASE_ADMIN_PROJECT_ID is missing");
-  }
-  if (!process.env.FIREBASE_ADMIN_CLIENT_EMAIL) {
-    console.error("FIREBASE_ADMIN_CLIENT_EMAIL is missing");
-  }
-  if (!process.env.FIREBASE_ADMIN_PRIVATE_KEY) {
-    console.error("FIREBASE_ADMIN_PRIVATE_KEY is missing");
-  }
-  if (!process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL) {
-    console.error("NEXT_PUBLIC_FIREBASE_DATABASE_URL is missing");
-  }
-  
+
   adminDb = admin.database();
 } else {
   adminDb = admin.app().database();
