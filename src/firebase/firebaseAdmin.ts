3ce56fe93d7872c5a8010fed731d6baea31de41d
kimchoker <kimchoker@naver.com>
2024-12-13 16:51:00 +0900

    admin

--- src/firebase/firebaseAdmin.ts
+++ src/firebase/firebaseAdmin.ts
@@ -1,13 +1,17 @@
-import admin from "firebase-admin";
+import admin, { ServiceAccount } from "firebase-admin";
 
 let adminDb: admin.database.Database | null = null;
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS || "{}");
+  const serviceAccount: ServiceAccount = {
+    projectId: process.env.FIREBASE_ADMIN_PROJECT_ID!,
+    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL!,
+  };
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!,
   });
 
   adminDb = admin.database();
